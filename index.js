1b3c2b35efffd9c960fbab59a10fd4d25f000973
Víctor Droguett <victor.droguett.mz@gmail.com>
2021-09-27 00:51:36 -0300

    configuraciones para desplegar en heroku

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(logger());
 Sentry.init({ dsn: "https://dd2f331f89ef435987811d9417e37f31@o940998.ingest.sentry.io/5979966" });
 
 app.on("error", (err, ctx) => {
-  Sentry.withScope(function(scope) {
-    scope.addEventProcessor(function(event) {
+  Sentry.withScope(function (scope) {
+    scope.addEventProcessor(function (event) {
       return Sentry.Handlers.parseRequest(event, ctx.request);
     });
     Sentry.captureException(err);
@@ -24,4 +24,4 @@ var movies = require('./routes/movies');
 app.use(movies.routes())
   .use(movies.allowedMethods());
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
